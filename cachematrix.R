a1f37795f9a89e5f65402fddff7a5205fac2869b
sofdemey <sofdemey@134.115.236.14.dyn.murdoch.edu.au>
2014-05-23 14:13:48 +0800

    Programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Assignment 2: Cache invert matrix - Sofdemey
+# Certain calculations  take a lot of time and computational power, however if you 
+# cache them you can recycle the result for further calculations.
 
-## Write a short comment describing this function
+# First you make an object containing the following:
+# 1. a numeric vector x containing an element m without any defined form, hence the 
+# NULL
+# 2. several subfunctions (set, get, setsolve, getsolve), which include: adding the 
+# inverse of the matrix to m and storing it in the cache.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
-
+# The second part is investigating first if the revert matrix is already in the 
+# cache and if not then it calculates the invert matrix and stores it in the cache.
+# when the invert matrix is already in the cache then you get the message 
+# "getting previous cached data"
+# if this is not the case then you will be notified of calculation and storage in cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting previous cached data")
+                return(m)
+        }
+        message("calculating invert matrix and storing in cache")
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
